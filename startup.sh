f698c08e804eb8b879c1b780334396607b4ba57c
dqkhanh <baldeagle9x@gmail.com>
2022-09-21 06:51:00 +0700

    add some software

---
+++ startup.sh
@@ -0,0 +1,167 @@
+echo "Welcome! Let's start setting up your system."
+
+echo "What name do you want to use in GIT user.name?"
+echo "For example, mine will be \"User Name\""
+read git_config_user_name
+
+echo "What email do you want to use in GIT user.email?"
+echo "For example, mine will be \"email@domain.com\""
+read git_config_user_email
+
+cd ~ && sudo apt-get update
+
+echo 'Installing curl' 
+sudo apt-get install curl -y
+
+echo 'Installing neofetch' 
+sudo apt-get install neofetch -y
+
+echo 'Installing tool to handle clipboard via CLI'
+sudo apt-get install xclip -y
+
+echo 'Installing latest git' 
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update && sudo apt-get install git -y
+
+echo 'Installing python3-pip'
+sudo apt-get install python3-pip -y
+
+
+echo "Setting up your git global user name and email"
+git config --global user.name "$git_config_user_name"
+git config --global user.email $git_config_user_email
+
+echo 'Cloning your .gitconfig from gist'
+getmy .gitconfig
+
+echo 'Generating a SSH Key'
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+echo 'Installing ZSH'
+sudo apt-get install zsh -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s $(which zsh)
+
+echo 'Indexing snap to ZSH'
+sudo chmod 777 /etc/zsh/zprofile
+echo "emulate sh -c 'source /etc/profile.d/apps-bin-path.sh'" >> /etc/zsh/zprofile
+
+echo 'Installing Spaceship ZSH Theme'
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sed 's/robbyrussell/spaceship/g' .zshrc
+source ~/.zshrc
+
+echo 'Installing Oh My Zsh'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo 'Installing FiraCode'
+sudo apt-get install fonts-firacode -y
+
+echo 'Installing Golang'
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+echo 'Installing NVM (Node Version Manager)' 
+sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
+
+export NVM_DIR="$HOME/.nvm" && (
+git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+source ~/.zshrc
+clear
+
+echo 'Installing NodeJS LTS'
+nvm --version
+nvm install --lts
+nvm current
+
+echo 'Installing Yarn'
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install --no-install-recommends yarn
+echo '"--emoji" true' >> ~/.yarnrc
+
+echo 'Installing Typescript, AdonisJS CLI and Lerna'
+yarn global add typescript @adonisjs/cli lerna
+clear
+
+echo 'Installing VSCode'
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+
+echo 'Installing Vivaldi Browser' 
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main' -y
+sudo apt update && sudo apt install vivaldi-stable
+
+echo 'Launching Vivaldi on Github so you can paste your keys'
+vivaldi https://github.com/settings/keys </dev/null >/dev/null 2>&1 & disown
+
+echo 'Installing Docker'
+curl -fsSL https://get.docker.com -o get-docker.sh
+DRY_RUN=1 sh ./get-docker.sh
+sudo service docker start
+# Configure Docker to start on boot
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+# To disable this behavior, use disable instead.
+# sudo systemctl disable docker.service
+# sudo systemctl disable containerd.service
+docker --version
+# Manage Docker as a non-root user
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo chmod 777 /var/run/docker.sock
+
+
+echo 'Installing Insomnia Core and Omni Theme' 
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
+  | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+  | sudo apt-key add -
+sudo apt-get update && sudo apt-get install insomnia -y
+mkdir ~/.config/Insomnia/plugins && cd ~/.config/Insomnia/plugins
+git clone https://github.com/Rocketseat/insomnia-omni.git omni-theme && cd ~
+
+echo 'Installing VLC'
+sudo apt-get install vlc -y
+sudo apt-get install vlc-plugin-access-extra libbluray-bdj libdvdcss2 -y
+
+echo 'Installing Discord'
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo dpkg -i discord.deb
+sudo apt-get install -f -y && rm discord.deb
+
+echo 'Installing Zoom'
+wget -c https://zoom.us/client/latest/zoom_amd64.deb
+sudo dpkg -i zoom_amd64.deb
+sudo apt-get install -f -y && rm zoom_amd64.deb
+
+echo 'Installing Peek' 
+sudo add-apt-repository ppa:peek-developers/stable -y
+sudo apt-get update && sudo apt-get install peek -y
+
+echo 'Updating and Cleaning Unnecessary Packages'
+sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get full-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
+clear
+
+echo 'Bumping the max file watchers'
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+echo 'Done!'
